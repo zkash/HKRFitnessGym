28999d605b6a2e0a41a0a03a83f55262448fb369
shameer <>
2017-04-11 22:43:27 +0200

    Add GUI elements to the CreateUserPageController

--- HKRFitnessGym/src/com/Project/Controllers/CreateUserPageController.java
+++ HKRFitnessGym/src/com/Project/Controllers/CreateUserPageController.java
@@ -9,6 +9,11 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -20,6 +25,30 @@ public class CreateUserPageController implements Initializable {
     /**
      * Initializes the controller class.
      */
+    
+    @FXML private TextField firstName;
+    @FXML private TextField middleName;
+    @FXML private TextField lastName;
+    @FXML private TextField address;
+    @FXML private TextField phoneNumber;
+    @FXML private TextField email;
+    @FXML private TextField ssn;
+    @FXML private TextField username;
+    @FXML private TextField password;
+    @FXML private RadioButton genderMale;
+    @FXML private RadioButton genderFemale;
+    @FXML private RadioButton genderOther;
+    @FXML private DatePicker dateOfBirth;
+   
+    @FXML private Label invalidMsgFirstName;
+    @FXML private Label invalidMsgMiddleName;
+    @FXML private Label invalidMsgLastName;
+    @FXML private Label invalidMsgAddress;
+    @FXML private Label invalidMsgPhoneNumber;
+    @FXML private Label invalidMsgEmail;
+    @FXML private Label invalidMsgSSN;
+    @FXML private Label invalidMsgUsername;
+    @FXML private Label invalidMsgPassword;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {

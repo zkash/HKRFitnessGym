dfbd973ce688460b11a0ddb6032595b950b5dd93
shameer <shameer@localhost>
2017-04-11 22:44:41 +0200

    Add GUI elements to the CreatePackagePageController

--- HKRFitnessGym/src/com/Project/Controllers/CreatePackagePageController.java
+++ HKRFitnessGym/src/com/Project/Controllers/CreatePackagePageController.java
@@ -9,6 +9,10 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -20,6 +24,20 @@ public class CreatePackagePageController implements Initializable {
     /**
      * Initializes the controller class.
      */
+    
+    @FXML private Label invalidMsgPackageName;
+    @FXML private Label invalidMsgPackageCost;
+    @FXML private Label invalidMsgPackageStartTime;
+    @FXML private Label invalidMsgPackageStartTimeState;
+    @FXML private TextField packageName;
+    @FXML private TextField packageCost;
+    @FXML private DatePicker packageStartDate;
+    @FXML private DatePicker packageEndDate;
+    @FXML private TextField packageStartTime;
+    @FXML private ComboBox packageStartTimeState;
+    @FXML private TextField packageDuration;
+    
+    ObservableList<String> timeList = FXCollections.observableArrayList("AM", "PM");
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {

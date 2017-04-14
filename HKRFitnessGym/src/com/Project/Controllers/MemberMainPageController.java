4d110e8ec4ea686ce1fd552eb5e9e00f64f9d480
shameer <shameer@localhost>
2017-04-14 19:59:39 +0200

    Fix Back Button functionality in all scenes

--- HKRFitnessGym/src/com/Project/Controllers/MemberMainPageController.java
+++ HKRFitnessGym/src/com/Project/Controllers/MemberMainPageController.java
@@ -5,9 +5,16 @@
  */
 package com.Project.Controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -23,5 +30,14 @@ public class MemberMainPageController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    
+    public void signOutBtnClick(ActionEvent event) throws IOException {
+        Node node = (Node) event.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("/com/Project/FXML/LoginPage.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
     
 }

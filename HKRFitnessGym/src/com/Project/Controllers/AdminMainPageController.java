dc05d2235cdf464c1a9cac65eb50a31cd3ba47b8
shameer <None>
2017-04-14 22:00:26 +0200

    Add main page for admin and its controller logic

---
+++ HKRFitnessGym/src/com/Project/Controllers/AdminMainPageController.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Project.Controllers;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author shameer
+ */
+public class AdminMainPageController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    public void signOutBtnClick(ActionEvent event) throws IOException {
+        Node node = (Node) event.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource("/com/Project/FXML/LoginPage.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    
+    public void accountBtnClick(ActionEvent event) throws IOException {
+    }
+    
+    public void packageBtnClick(ActionEvent event) throws IOException {
+    }
+    
+    public void scheduleBtnClick(ActionEvent event) throws IOException {
+    }
+    
+    public void announcementBtnClick(ActionEvent event) throws IOException {
+    }
+    
+}

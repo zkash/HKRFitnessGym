68c0d85c6ec886e8c3a6f1d0d13d62d644b2f588
zkash <zkash922@gmail.com>
2017-04-21 03:19:51 +0200

    Few modifications in Chat page fxml & controller files.

--- HKRFitnessGym/src/com/Project/JDBC/DAO/DBHandler.java
+++ HKRFitnessGym/src/com/Project/JDBC/DAO/DBHandler.java
@@ -20,12 +20,40 @@ import java.util.List;
  */
 
 public class DBHandler {
+    private static Connection c;
     private final String dbName = "HKRFitnessGymDB";
     private final String user = "root" ;
     private final String password = "root";
     private final String connectionURL = "jdbc:mysql://localhost/" + dbName  + "?user=" + user + "&password=" + password + "&useSSL=false";
     
-    public static void adminViewAccounts() throws SQLException {
+    // Connection to database.
+    public static void connect(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            //String URL
+            String URL = "jdbc:mysql://127.0.0.1:3306/hkrfitnessgym?user=root&password=root";
+            c = DriverManager.getConnection(URL);
+            if (c != null)
+                System.out.println("Connected to the database");
+        }
+        catch (ClassNotFoundException | InstantiationException | IllegalAccessException | SQLException ex){
+                        
+        }
+    }
+    
+    // To Disconnect the database.
+        public static void shutDown(){
+            try{
+                c.close();
+            }
+            catch (SQLException ex){
+        }
+    }
+    
+    
+    
+    
+    /*public static void adminViewAccounts() throws SQLException {
         Connection conn = establishConnection();
         ResultSet rs = null;
         ArrayList<ArrayList<String>> finalArray = new ArrayList<>();
@@ -73,5 +101,5 @@ public class DBHandler {
             System.out.println("Not connected to database");
         }
         return null;
-    }
+    }*/
 }

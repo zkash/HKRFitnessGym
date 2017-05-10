e8226fc4c82fc13463b432e2c900e2ba8b62b938
shameer <shprnprog@gmail.com>
2017-05-10 20:03:41 +0200

    Refactor AdminMainPageController

--- HKRFitnessGym/src/com/Project/Controllers/AdminMainPageController.java
+++ HKRFitnessGym/src/com/Project/Controllers/AdminMainPageController.java
@@ -5,13 +5,8 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -32,61 +27,69 @@ public class AdminMainPageController implements Initializable {
         loggedUserLbl.textProperty().bind(LoginStorage.getInstance().usernameProperty());
     } 
     
+    
+    /**
+     * Navigate to Create User page
+     * @param event
+     * @throws IOException 
+     */
     @FXML
     public void accountBtnClick(ActionEvent event) throws IOException {
-      Node node = (Node) event.getSource();
-      Stage stage = (Stage) node.getScene().getWindow();
-      Parent root = FXMLLoader.load(getClass().getResource("/com/Project/FXML/CreateUserPage.fxml"));
-      Scene scene = new Scene(root);
-      stage.setScene(scene);
-      stage.show();
-    }
-    @FXML
-    public void packageBtnClick(ActionEvent event) throws IOException {
-      Node node = (Node) event.getSource();
-      Stage stage = (Stage) node.getScene().getWindow();
-      Parent root = FXMLLoader.load(getClass().getResource("/com/Project/FXML/CreatePackagePage.fxml"));
-      Scene scene = new Scene(root);
-      stage.setScene(scene);
-      stage.show();
-        
-    }
-    @FXML
-    public void scheduleBtnClick(ActionEvent event) throws IOException {
-      Node node = (Node) event.getSource();
-      Stage stage = (Stage) node.getScene().getWindow();
-      Parent root = FXMLLoader.load(getClass().getResource("/com/Project/FXML/CreateSchedulePage.fxml"));
-      Scene scene = new Scene(root);
-      stage.setScene(scene);
-      stage.show();
-    }
-    @FXML
-    public void announcementBtnClick(ActionEvent event) throws IOException {
-      Node node = (Node) event.getSource();
-      Stage stage = (Stage) node.getScene().getWindow();
-      Parent root = FXMLLoader.load(getClass().getResource("/com/Project/FXML/CreateAnnouncementPage.fxml"));
-      Scene scene = new Scene(root);
-      stage.setScene(scene);
-      stage.show();
-    }
-   
-    @FXML
-    public void chatBtnClick(ActionEvent event) throws IOException {
-      Node node = (Node) event.getSource();
-      Stage stage = (Stage) node.getScene().getWindow();
-      Parent root = FXMLLoader.load(getClass().getResource("/com/Project/FXML/ChatPage.fxml"));
-      Scene scene = new Scene(root);
-      stage.setScene(scene);
-      stage.show();
-    }
-    @FXML
-    public void subscribeBtnClick(ActionEvent event) throws IOException {
-      Node node = (Node) event.getSource();
-      Stage stage = (Stage) node.getScene().getWindow();
-      Parent root = FXMLLoader.load(getClass().getResource("/com/Project/FXML/AdminViewMemberAccounts.fxml"));
-      Scene scene = new Scene(root);
-      stage.setScene(scene);
-      stage.show();
+        helper.navigateScene(event, "CreateUserPage.fxml");
     }
     
+    
+    /**
+     * Navigate to Create Package page
+     * @param event
+     * @throws IOException 
+     */
+    @FXML
+    public void packageBtnClick(ActionEvent event) throws IOException {
+      helper.navigateScene(event, "CreatePackagePage.fxml");   
+    }
+    
+    
+    /**
+     * Navigate to Create Schedule page
+     * @param event
+     * @throws IOException 
+     */
+    @FXML
+    public void scheduleBtnClick(ActionEvent event) throws IOException {
+        helper.navigateScene(event, "CreateSchedulePage.fxml");   
+    }
+    
+    
+    /**
+     * Navigate to Create Announcement page
+     * @param event
+     * @throws IOException 
+     */
+    @FXML
+    public void announcementBtnClick(ActionEvent event) throws IOException {
+        helper.navigateScene(event, "CreateAnnouncementPage.fxml");   
+    }
+   
+    
+    /**
+     * Navigate to Chat page
+     * @param event
+     * @throws IOException 
+     */
+    @FXML
+    public void chatBtnClick(ActionEvent event) throws IOException {
+        helper.navigateScene(event, "ChatPage.fxml");   
+    }
+    
+    
+    /**
+     * Navigate to View Member Accounts page
+     * @param event
+     * @throws IOException 
+     */
+    @FXML
+    public void subscribeBtnClick(ActionEvent event) throws IOException {
+        helper.navigateScene(event, "AdminViewMemberAccounts.fxml");   
+    } 
 }

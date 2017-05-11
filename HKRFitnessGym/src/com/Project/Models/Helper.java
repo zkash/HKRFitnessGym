013d7d945431af33461deb0327d1fd89e8b3a4d0
shameer <shprnprog@gmail.com>
2017-05-11 22:28:27 +0200

    Change path location to 'Views' instead of 'FXML' in navigateScene method in Helper class

--- HKRFitnessGym/src/com/Project/Models/Helper.java
+++ HKRFitnessGym/src/com/Project/Models/Helper.java
@@ -206,7 +206,7 @@ public class Helper {
     public void navigateScene(ActionEvent event, String nextSceneLocation) throws IOException {
         Node node = (Node) event.getSource();
         Stage stage = (Stage) node.getScene().getWindow();
-        Parent root = FXMLLoader.load(Helper.class.getResource("/com/Project/FXML/" + nextSceneLocation));
+        Parent root = FXMLLoader.load(Helper.class.getResource("/com/Project/Views/" + nextSceneLocation));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();

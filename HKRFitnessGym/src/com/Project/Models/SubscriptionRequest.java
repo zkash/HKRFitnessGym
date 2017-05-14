7ad67f888ecf8b54fe98f939e12a9e37788449fd
shameer <shprnprog@gmail.com>
2017-05-14 13:12:57 +0200

    Refactor SubscriptionRequest class and add comments in it

--- HKRFitnessGym/src/com/Project/Models/SubscriptionRequest.java
+++ HKRFitnessGym/src/com/Project/Models/SubscriptionRequest.java
@@ -1,15 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.Project.Models;
 
 import java.sql.Date;
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
 
 /**
  *
@@ -19,30 +10,58 @@ public class SubscriptionRequest extends Subscription {
     private String memberFullName = null;
     private String memberUsername = null;
  
-    
-    
+    /**
+     * Constructor for getSubscriptionRequest, searchInAdminViewSubscriptionRequests
+     * @param packageName Name of package
+     * @param price Price of package
+     * @param packageStartDate Start date of package
+     * @param packageEndDate End date of package
+     * @param startTime Start time of package in a day
+     * @param endTime End time of package in a day
+     * @param subscriptionStartDate Start date of subscription
+     * @param subscriptionEndDate End date of subscription
+     * @param subscriptionId Id of subscription
+     */
     public SubscriptionRequest(String packageName, Float price, Date packageStartDate, Date packageEndDate, String startTime, String endTime, Date subscriptionStartDate, Date subscriptionEndDate, int subscriptionId) {
             super(packageName, price, packageStartDate, packageEndDate, startTime, endTime, subscriptionStartDate, subscriptionEndDate, subscriptionId);
     }
     
+    
+    /**
+     * Accessor method for memberFullName
+     * @return Full name of member
+     */
+    @Override
     public String getMemberFullName() {
         return memberFullName;
     }
     
+    
+    /**
+     * Accessor method for memberUsername
+     * @return Username of member
+     */
     public String getSubscriberUsername() {
         return memberUsername;
     }
     
 
-
-    public void setMemberFullName(String mfn) {
-        this.memberFullName = mfn;
+    /**
+     * Mutator method for memberFullName
+     * @param memberFullName Full name of member
+     */
+    @Override
+    public void setMemberFullName(String memberFullName) {
+        this.memberFullName = memberFullName;
     }
     
-    public void setMemberUsername(String mun) {
-        this.memberUsername = mun;
-    }
     
-
-  
+    /**
+     * Mutator method for memberUsername
+     * @param memberUsername Username of member
+     */
+    @Override
+    public void setMemberUsername(String memberUsername) {
+        this.memberUsername = memberUsername;
+    }
 }

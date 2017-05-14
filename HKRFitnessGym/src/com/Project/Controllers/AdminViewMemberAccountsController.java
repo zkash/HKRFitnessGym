1248ecd308af5589a25e9927583f92d00cd57c4d
shameer <shprnprog@gmail.com>
2017-05-14 15:42:46 +0200

    Refactor AdminViewAdminAccountsController and AdminViewMemberAccountsController and add comments in them

--- HKRFitnessGym/src/com/Project/Controllers/AdminViewMemberAccountsController.java
+++ HKRFitnessGym/src/com/Project/Controllers/AdminViewMemberAccountsController.java
@@ -1,20 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.Project.Controllers;
 
 import com.Project.Models.DBHandler;
 import com.Project.Models.Helper;
 import com.Project.Models.Member;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -33,14 +26,6 @@ import javafx.stage.Stage;
  * @author shameer
  */
 public class AdminViewMemberAccountsController implements Initializable {
-
-    /**
-     * Initializes the controller class.
-     */
-    
-    private int adminSSN;
-    private boolean login;
-    
     @FXML private TableView<Member> adminViewAccountsTable;
     @FXML private TableColumn<Member, String> fullNameColumn;
     @FXML private TableColumn<Member, String> usernameColumn;
@@ -51,9 +36,7 @@ public class AdminViewMemberAccountsController implements Initializable {
     @FXML private TableColumn<Member, String> emailColumn;
     @FXML private TableColumn<Member, String> phoneNumberColumn;
     @FXML private TableColumn<Member, String> adminFullNameColumn;
-    
-    private  ObservableList<Member> data;
-    private  ObservableList<Member> searchData;
+   
     @FXML private TextField searchMember;
     
     @FXML private CheckBox searchFullName;
@@ -65,62 +48,72 @@ public class AdminViewMemberAccountsController implements Initializable {
     
     private final DBHandler dbHandler = new DBHandler();
     private final Helper helper = new Helper();
-    
+        
+    private  ObservableList<Member> data;
+   
+    /**
+     * Initializes the controller class.
+     * @param url
+     * @param rb
+     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         try {
-            
-            ArrayList<ArrayList<String>> finalArray;
             data = dbHandler.adminViewMemberAccounts();
-            setDataInTable(data);
-            
-        } catch (SQLException ex) {
-            Logger.getLogger(AdminViewMemberAccountsController.class.getName()).log(Level.SEVERE, null, ex);
+            setDataInTable(data);   
+        } 
+        catch (SQLException ex) {
+            helper.showDialogBox(true, "Data could not be fetched from database and set in table");
         }
     }
     
-    public void viewDetailsBtnClick(ActionEvent event) {
-        //String selected = adminViewAccountsTable.selectionModel.getSelectedItem();
-       
-    }
-    
+ 
+    /**
+     * Deletes a member account
+     * @param event
+     * @throws SQLException
+     * @throws IOException 
+     */
     public void deleteBtnClick(ActionEvent event) throws SQLException, IOException {
         ObservableList<Member> row , allRows;
         allRows = adminViewAccountsTable.getItems();
         row = adminViewAccountsTable.getSelectionModel().getSelectedItems(); 
-        boolean deletionError = true;
+        
         if (row.isEmpty()) {
-            helper.showDialogBox(deletionError, "Please select an member account first to delete the account");
+            helper.showDialogBox(true, "Please select an member account first to delete the account");
         }
         else {
             Node node = (Node) event.getSource();
             Stage stage = (Stage) node.getScene().getWindow();
+            
             helper.showDialogBoxChoice(stage, "Confirm Deletion", "Are you sure you want to delete the member account?", "com/Project/FXML/AdminViewAdminAccounts.fxml");
+            
             try {
                 String[] fullSSN = (row.get(0).getFullSSN()).split("-");
                 int ssn1 = Integer.parseInt(fullSSN[0]);
                 int ssn2 = Integer.parseInt(fullSSN[1]);
                 String username = row.get(0).getUsername();
+                
                 dbHandler.deleteAccount(ssn1, ssn2, username, "Member");
-            }
-            catch(NumberFormatException e) {
-                deletionError = true;
-            }
-
-            if (!deletionError) {
-                helper.showDialogBox(deletionError, "Member successfully deleted");
+                helper.showDialogBox(false, "Member successfully deleted");
                 row.forEach(allRows::remove);
             }
-            else {
-                helper.showDialogBox(deletionError, "Could not delete member because it is associated with other data in the system. \n\nDelete such data before trying to delete the member");
-            }  
+            catch(NumberFormatException | SQLException ex) {
+                helper.showDialogBox(true, "Could not delete admin because it is associated with other data in the system. \n\nDelete such data before trying to delete the admin");
+            }
         }
     }
     
-    public void searchMemberBtnClick(ActionEvent event) throws SQLException {
+    
+    /**
+     * Searches for data as per user's query and filters
+     * @param event
+     * @throws SQLException 
+     * @throws java.lang.reflect.InvocationTargetException 
+     */
+    public void searchMemberBtnClick(ActionEvent event) throws SQLException, IllegalArgumentException, InvocationTargetException {
         String searchQuery = searchMember.getText(); 
-        
-        
+
         String fn = null, mn = null, ln = null, add = null, un = null, ead = null;
         int pnum = -1, ssn1 = -1, ssn2 = -1;
         
@@ -144,11 +137,10 @@ public class AdminViewMemberAccountsController implements Initializable {
         
         if(searchPhoneNumber.isSelected()) {
             if(helper.hasChar(searchQuery)) {
-                //Helper.showDialogBox(true, "Cannot search for text in phone number");
+                helper.showDialogBox(true, "Cannot search for text in phone number");
             }
             else {
                 try {
-                    System.out.println("sfjkhdfs");
                     pnum = Integer.parseInt(searchQuery);
                 }
                 catch (NumberFormatException e) {
@@ -159,17 +151,18 @@ public class AdminViewMemberAccountsController implements Initializable {
         
         if(searchSSN.isSelected()) {
             String ssnRegex = "([0-9]{6}-[0-9]{4})|([0-9]{10})";
+            
             if (searchQuery.matches(ssnRegex)) {
-                //try {
-                    String[] ssnDivided = searchQuery.split("-");
-                    if(ssnDivided.length == 1) {
-                        ssn1 = Integer.parseInt(searchQuery.substring(0, 6));
-                        ssn2 = Integer.parseInt(searchQuery.substring(6, 10));
-                    }
-                    else if (ssnDivided.length == 2) {
-                        ssn1 = Integer.parseInt(ssnDivided[0]);
-                        ssn2 = Integer.parseInt(ssnDivided[1]);
-                    }
+                String[] ssnDivided = searchQuery.split("-");
+                
+                if(ssnDivided.length == 1) {
+                    ssn1 = Integer.parseInt(searchQuery.substring(0, 6));
+                    ssn2 = Integer.parseInt(searchQuery.substring(6, 10));
+                }
+                else if (ssnDivided.length == 2) {
+                    ssn1 = Integer.parseInt(ssnDivided[0]);
+                    ssn2 = Integer.parseInt(ssnDivided[1]);
+                }
             }
             else {
                 helper.showDialogBox(true, "Search query does not match SSN format (either 10 digits or 6 digits followed by - and 4 digits");
@@ -188,40 +181,30 @@ public class AdminViewMemberAccountsController implements Initializable {
             un = searchQuery;
         }
         
-        searchData = dbHandler.searchInAdminViewMemberAccounts(fn, mn, ln, add, un, ead, pnum, ssn1, ssn2, "Member");
-        
-        adminViewAccountsTable.getColumns().clear();
-        fullNameColumn = new TableColumn("Full Name");
-        usernameColumn = new TableColumn("Username");
-        ssnColumn = new TableColumn("SSN");
-        dobColumn = new TableColumn("DOB");
-        genderColumn = new TableColumn("Gender");
-        addressColumn = new TableColumn("Address");
-        emailColumn = new TableColumn("Email");
-        phoneNumberColumn = new TableColumn("Phone");
-        adminFullNameColumn = new TableColumn("Admin");
-          
-        adminViewAccountsTable.getColumns().addAll(fullNameColumn, usernameColumn, ssnColumn, genderColumn, dobColumn, addressColumn, emailColumn, phoneNumberColumn, adminFullNameColumn);
-        fullNameColumn.prefWidthProperty().bind(adminViewAccountsTable.widthProperty().multiply(0.30837004)); 
-        usernameColumn.prefWidthProperty().bind(adminViewAccountsTable.widthProperty().multiply(0.17621146));
-        ssnColumn.prefWidthProperty().bind(adminViewAccountsTable.widthProperty().multiply(0.17621146));
-        dobColumn.prefWidthProperty().bind(adminViewAccountsTable.widthProperty().multiply(0.17621146));
-        genderColumn.prefWidthProperty().bind(adminViewAccountsTable.widthProperty().multiply(0.10572688));
-        addressColumn.prefWidthProperty().bind(adminViewAccountsTable.widthProperty().multiply(0.40));
-        emailColumn.prefWidthProperty().bind(adminViewAccountsTable.widthProperty().multiply(0.40));
-        phoneNumberColumn.prefWidthProperty().bind(adminViewAccountsTable.widthProperty().multiply(0.17621146));
-        adminFullNameColumn.prefWidthProperty().bind(adminViewAccountsTable.widthProperty().multiply(0.17621146));
-        
-        setDataInTable(searchData);
+        data = dbHandler.searchInAdminViewMemberAccounts(fn, mn, ln, add, un, ead, pnum, ssn1, ssn2, "Member");
+        setDataInTable(data);
+        helper.fitColumns(adminViewAccountsTable);  
     }
     
-    public void resetSearchBtnClick(ActionEvent event) throws SQLException {
+    
+    /**
+     * Resets the table view with initial data
+     * @param event
+     * @throws SQLException 
+     */
+    public void resetSearchBtnClick(ActionEvent event) throws SQLException, IllegalArgumentException, InvocationTargetException {
         data = dbHandler.adminViewMemberAccounts();
         setDataInTable(data);
+        helper.fitColumns(adminViewAccountsTable);
     }
     
+    
+    /**
+     * Sets data in table view
+     * @param data ObservableList of Member object
+     * @throws IllegalArgumentException 
+     */
     public void setDataInTable(ObservableList<Member> data) {
-        // Set cell value factory to TableView
         fullNameColumn.setCellValueFactory(new PropertyValueFactory<>("fullName"));
         usernameColumn.setCellValueFactory(new PropertyValueFactory<>("username"));
         ssnColumn.setCellValueFactory(new PropertyValueFactory<>("fullSSN"));
@@ -231,7 +214,6 @@ public class AdminViewMemberAccountsController implements Initializable {
         emailColumn.setCellValueFactory(new PropertyValueFactory<>("email"));
         phoneNumberColumn.setCellValueFactory(new PropertyValueFactory<>("phoneNumber"));
         adminFullNameColumn.setCellValueFactory(new PropertyValueFactory<>("adminFullName"));
-        adminViewAccountsTable.setItems(null);
         adminViewAccountsTable.setItems(data);
-        }
+    }
 }

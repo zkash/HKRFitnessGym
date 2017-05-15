fb76bf55be62dbdef2897c44bc2e9ffeeb773296
zkash <zkash922@gmail.com>
2017-05-15 15:03:16 +0200

    Few modifications in AdminChatController.java.

--- HKRFitnessGym/src/com/Project/Controllers/AdminChatController.java
+++ HKRFitnessGym/src/com/Project/Controllers/AdminChatController.java
@@ -5,12 +5,13 @@
  */
 package com.Project.Controllers;
 
+import com.Project.Models.Chat;
 import com.Project.Models.DBHandler;
-
+import com.Project.Models.Helper;
 import com.Project.Models.LoginStorage;
 import java.net.URL;
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.time.LocalTime;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -27,42 +28,60 @@ public class AdminChatController implements Initializable {
     @FXML
     private TextArea messageArea;
     @FXML
-    private TextArea message;
+    private TextArea text;
     @FXML
     private Label errorMessage;
     @FXML
     private Button enter;
     
-    private DBHandler dbHandler = new DBHandler();
-    private int id =  LoginStorage.getInstance().getId();
+    //private int id =  LoginStorage.getInstance().getId();
     private String accountType = LoginStorage.getInstance().getAccountType();
+    private final Helper helper = new Helper();
+    private final DBHandler dbHandler = new DBHandler();
     
     @Override
     public void initialize(URL location, ResourceBundle resources) {
   }
     @FXML
     private void saveMessage(ActionEvent event) {
-        Date date = new Date();
-        SimpleDateFormat datef = new SimpleDateFormat("yyyy.MM.dd 'at' H:mm ");
-        try {
-            // Checks if field is empty.
-            if (message.getText().length() == 0) {
-                errorMessage.setText("Please write your message.");
+        //Date date = new Date();
+        SimpleDateFormat dateformat = new SimpleDateFormat("yyyy.MM.dd 'at' H:mm ");
+        java.sql.Date date = helper.getCurrentDateInSqlDate();
+        System.out.println("DATE " + date);
+        LocalTime currentTime= helper.getCurrentTime();
+        System.out.println("CT " + currentTime);
+        //java.sql.Date sqlDate = (java.sql.Date) date;
+       // String title = titleTextField.getText();
+       try{
+            // It checks if field is unfilled.
+            if(text.getText().length()== 0){
+                errorMessage.setText("Please write your message");
             }
-            // Checks the lenght of the message.
-            else if (message.getText().length() >= 100) {
-                errorMessage.setText("Maximum 100 characters");
-                message.clear();
+            // checks the length of text.
+            else if(text.getText().length() >= 100){
+                errorMessage.setText("Maximum 100 characters allowed");
+                text.clear();
             }
-            // If none of conditions apply, message will be saved.
-            else if (message.getText().length() <= 100 && message.getText().length() > 0) {
-                //DBHandler.saveMessage(datef.format(date), DBHandler.getLoggedUser(), message.getText());
-                message.clear();
-           }
-        } catch (Exception e) {
+            // Saves text after fulfill conditions.
+            else if(text.getText().length() <= 100 && text.getText().length() > 0){
+                //dbHandler.saveAnnouncement(dateformat.format(date), text.getText());
+          
+                String txt = text.getText();
+                Chat chat = new Chat();
+                chat.setDate(date);
+                chat.setTime(currentTime.toString());
+             // chat.setMessage(message);
+             // chat.setMessageId(messageId);
+              //chat.setSenderUsername(senderUsername);
+             // chat.setReceiveUsername(receiveUsername);
+                //dbHandler.saveMessage(chat);
+                text.clear();
+                
+            }
+            
+       }catch (Exception e){
             e.printStackTrace();
         }
     }
-
-}
     
+}

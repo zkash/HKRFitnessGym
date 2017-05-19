05e020cd0e91cdb60099dcf02b53bf08d4c7b1c0
zkash <zkash922@gmail.com>
2017-05-19 17:15:50 +0200

    Modifications in controller and fxml files admin view announcement.

--- src/com/Project/Controllers/AdminViewAnnouncementController.java
+++ src/com/Project/Controllers/AdminViewAnnouncementController.java
@@ -11,13 +11,17 @@ import com.Project.Models.Announcement;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
+import java.sql.Date;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
@@ -42,8 +46,15 @@ public class AdminViewAnnouncementController implements Initializable {
     private final DBHandler dbHandler = new DBHandler();
     private final Helper helper = new Helper();
     
-    private  ObservableList<Announcement> data;
+    private  ObservableList<Announcement> data;    
+    
+     @FXML private UpdateAdminAnnouncementController updateAdminAnnouncementController;
 
+     /**
+     * Initializes the controller class.
+     * @param url Uniform Resource Locator
+     * @param rb ResourceBundle
+     */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         try {
@@ -62,7 +73,7 @@ public class AdminViewAnnouncementController implements Initializable {
      * @throws IllegalArgumentException
      * @throws InvocationTargetException 
      */
-    public void handleSearchBtnClick(ActionEvent event) throws SQLException, IllegalArgumentException, InvocationTargetException {
+    public void searchBtnClick(ActionEvent event) throws SQLException, IllegalArgumentException, InvocationTargetException {
         String searchQuery = searchAnnouncement.getText();
         data = dbHandler.searchInAdminViewAnnouncement(searchQuery);
         setDataInTable(data);
@@ -77,7 +88,7 @@ public class AdminViewAnnouncementController implements Initializable {
      * @throws IllegalArgumentException
      * @throws InvocationTargetException 
      */
-    public void handleResetSearchBtnClick(ActionEvent event) throws SQLException, IllegalArgumentException, InvocationTargetException {
+    public void resetSearchBtnClick(ActionEvent event) throws SQLException, IllegalArgumentException, InvocationTargetException {
         data = dbHandler.adminViewAnnouncement();
         setDataInTable(data);
         helper.fitColumns(adminViewAnnouncementTable); 
@@ -90,7 +101,7 @@ public class AdminViewAnnouncementController implements Initializable {
      * @throws SQLException
      * @throws IOException 
      */
-    public void handleDeleteBtnClick(ActionEvent event) throws SQLException, IOException {
+    public void deleteBtnClick(ActionEvent event) throws SQLException, IOException {
         ObservableList<com.Project.Models.Announcement> row , allRows;
         allRows = adminViewAnnouncementTable.getItems();
         row = adminViewAnnouncementTable.getSelectionModel().getSelectedItems(); 
@@ -105,7 +116,7 @@ public class AdminViewAnnouncementController implements Initializable {
             helper.showDialogBoxChoice(stage, "Confirm Deletion", "Are you sure you want to delete the Announcemnt?", "com/Project/Views/AdminViewAnnouncemnt.fxml");
             try {
                 dbHandler.deleteAnnouncement(row.get(0).getTitle(), row.get(0).getBody(), row.get(0).getTime(), row.get(0).getDate() );
-                helper.showDialogBox(false, "Package successfully deleted");
+                helper.showDialogBox(false, "Announement successfully deleted");
                 row.forEach(allRows::remove);
             }
             catch(SQLException e) {
@@ -113,11 +124,43 @@ public class AdminViewAnnouncementController implements Initializable {
                // helper.showDialogBox(true, "Could not delete package because it is associated with other data in the system. \n\nDelete such data before trying to delete the package");
             }
         }
-        
-      //  public void updateAnnouncementBtnClick(ActionEvent event)throws SQLException, IOException {
+    }
+     /**
+     * Navigates to Update Package Information page
+     * @param event ActionEvent
+     * @throws SQLException
+     * @throws IOException 
+     */
+        @FXML
+        public void handleUpdateBtnClick(ActionEvent event)throws SQLException, IOException {
+        ObservableList<com.Project.Models.Announcement> row = adminViewAnnouncementTable.getSelectionModel().getSelectedItems();
+        if (row.isEmpty()) {
+            helper.showDialogBox(true, "Please select an announcement to update");
+        } else {
             
+        String title = row.get(0).getTitle();
+        String body = row.get(0).getBody();
+        String time = row.get(0).getTime();
+        Date date = row.get(0).getDate();
+        data = dbHandler.adminViewAnnouncement();
+              
+        Node node = (Node) event.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/Project/Views/UpdateAdminAnnouncement.fxml"));
+        Parent root = (Parent)loader.load();
+               
+        updateAdminAnnouncementController = loader.getController();
+            System.out.println(title);
+            System.out.println(body);
+            System.out.println(date);
+            System.out.println(time);
+        updateAdminAnnouncementController.setAnnouncement(title, body, date, time);
+            
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
         }
-    
+    }
     /**
      * Sets data in table view
      * @param data
@@ -127,8 +170,7 @@ public class AdminViewAnnouncementController implements Initializable {
 
     public void setDataInTable(ObservableList<Announcement> data) throws IllegalArgumentException, InvocationTargetException {
         // Set cell value factory to TableView
-        adminColumn.setCellValueFactory(new PropertyValueFactory<>("username"));
-        
+        adminColumn.setCellValueFactory(new PropertyValueFactory<>("username"));        
         dateColumn.setCellValueFactory(new PropertyValueFactory<>("date"));
         timeColumn.setCellValueFactory(new PropertyValueFactory<>("time"));
         titleColumn.setCellValueFactory(new PropertyValueFactory<>("title"));
